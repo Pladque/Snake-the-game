2c627ac0f71d3977ae1514133aa8792320159451
ThisNameIsNotAvailable <aliaxeyka@gmail.com>
2021-05-09 16:27:54 +0200

    added sound for eating an apple

--- main.cpp
+++ main.cpp
@@ -19,6 +19,25 @@ int snake_y = GRID_SIZE_Y / 2;
 int score = 0;
 sf::Text text;      // for displaying score
 
+sf::Texture snakeTexture;
+sf::Sprite snakeSP;
+sf::Texture headTexture;
+sf::Sprite head;
+sf::Texture appleTexture;
+sf::Sprite appleSP;
+
+
+
+void initGame(sf::SoundBuffer& appleEatingSound, sf::Sound& appleEating) {
+    snakeTexture.loadFromFile("snake.png");
+    snakeSP.setTexture(snakeTexture);
+    headTexture.loadFromFile("head.png");
+    head.setTexture(headTexture);
+    appleTexture.loadFromFile("apple.png");
+    appleSP.setTexture(appleTexture);
+    appleEatingSound.loadFromFile("applebite.wav");
+    appleEating.setBuffer(appleEatingSound);
+}
 void resizeSnake(Snake& snake, int size){
     for(int i = 0; i < size; i++)
         snake.grow();
@@ -33,12 +52,16 @@ void updateScore(int addToScore){
 // mby rework it, to check collision with array of
 //collectableObj - it will allow us to add fe poison apple, bombs etc
 // and other staff that snake can collide with
-void snakeHeadCollision(Snake *snake, collectableObj *obj1){
+
+void snakeHeadCollision(Snake *snake, collectableObj *obj1, sf::Sound& appleEating){
+    
     
     if(snake->getHead()->x == obj1->getPosX() &&  snake->getHead()->y == obj1->getPosY() ){
 //        std::cout<<score<<std::endl;
         // we can add golden aplles, that will f.e add 2 to size
         // and 5 to score
+        
+        appleEating.play();
         resizeSnake(*snake, obj1->getSizeBonus());
         updateScore(obj1->getScoreBonus());
         obj1->goToFreeRandomPosistion(snake->getHead());
@@ -48,20 +71,7 @@ void snakeHeadCollision(Snake *snake, collectableObj *obj1){
 void drawField(sf::RenderWindow& window, Snake& snake, 
                 collectableObj& collObj)// sf::Text &text) 
 {  
-    sf::Texture snakeTexture;
-    snakeTexture.loadFromFile("snake.png");
-    sf::Sprite snakeSP;
-    snakeSP.setTexture(snakeTexture);
     
-    sf::Texture headTexture;
-    headTexture.loadFromFile("head.png");
-    sf::Sprite head;
-    head.setTexture(headTexture);
-
-    sf::Texture appleTexture;
-    appleTexture.loadFromFile("apple.png");
-    sf::Sprite appleSP;
-    appleSP.setTexture(appleTexture);
     
     bodyPart* curr = snake.getHead();
     
@@ -87,29 +97,6 @@ void drawField(sf::RenderWindow& window, Snake& snake,
 
 }
 
-
-
-void handleKey(Snake& snake) {
-    if(sf::Keyboard::isKeyPressed(sf::Keyboard::W)) {
-        if(snake.getDirection() == Direction::up || snake.getDirection() == Direction::down)
-            return;
-        snake.changeDirection(Direction::up);
-    }else if(sf::Keyboard::isKeyPressed(sf::Keyboard::S)) {
-        if(snake.getDirection() == Direction::down || snake.getDirection() == Direction::up)
-            return;
-        snake.changeDirection(Direction::down);
-    }else if(sf::Keyboard::isKeyPressed(sf::Keyboard::A)) {
-        if(snake.getDirection() == Direction::left || snake.getDirection() == Direction::right)
-            return;
-        snake.changeDirection(Direction::left);
-    }else if(sf::Keyboard::isKeyPressed(sf::Keyboard::D)) {
-        if(snake.getDirection() == Direction::right || snake.getDirection() == Direction::left)
-            return;
-        snake.changeDirection(Direction::right);
-    }
-}
-
-
 void setupScoreDisplayer( sf::Font &font)
 {
     text.setFont(font); // font is a sf::Font
@@ -124,8 +111,11 @@ void setupScoreDisplayer( sf::Font &font)
 int main(int, char const**)
 {
     // Init game
+    sf::SoundBuffer appleEatingSound;
+    sf::Sound appleEating;
+    appleEating.setVolume(40);
     sf::RenderWindow window(sf::VideoMode(window_width, window_height), "Snake Game", sf::Style::Titlebar | sf::Style::Close);
-    
+    initGame(appleEatingSound, appleEating);
     //loading font
     sf::Font font;
     if (!font.loadFromFile("cabin-sketch.bold.ttf"))
@@ -143,20 +133,46 @@ int main(int, char const**)
 
     collectableObj apple = collectableObj("apple", 0, 0, 1, 1);
     apple.goToFreeRandomPosistion();
-
+    
+    Direction newDir;
     while(window.isOpen()){
         sf::Event ev;
-        
+        newDir = snake.getDirection();
         while(window.pollEvent(ev)) {
-            
             if(ev.type == sf::Event::Closed) {
                 window.close();
             }else if(ev.key.code == sf::Keyboard::Escape) {
                 window.close();
+            }else if(ev.type == sf::Event::KeyPressed) {
+                switch (ev.key.code) {
+                    case sf::Keyboard::W:
+                        if(snake.getDirection() == Direction::up || snake.getDirection() == Direction::down)
+                            break;
+                        newDir = Direction::up;
+                        break;
+                    case sf::Keyboard::S:
+                        if(snake.getDirection() == Direction::down || snake.getDirection() == Direction::up)
+                            break;;
+                        newDir = Direction::down;
+                        break;
+                    case sf::Keyboard::A:
+                        if(snake.getDirection() == Direction::left || snake.getDirection() == Direction::right)
+                            break;
+                        newDir = Direction::left;
+                        break;
+                    case sf::Keyboard::D:
+                        if(snake.getDirection() == Direction::right || snake.getDirection() == Direction::left)
+                            break;
+                        newDir = Direction::right;
+                        break;
+                    default:
+                        break;
+                }
             }
         }
         
-        handleKey(snake);
+        snake.changeDirection(newDir);
+        
 
         if(!snake.move())
             std::cout<<"GAME OVER"<<std::endl;
@@ -165,11 +181,12 @@ int main(int, char const**)
         
         drawField(window, snake, apple);//, text);
 
-        snakeHeadCollision(&snake, &apple);
+        snakeHeadCollision(&snake, &apple, appleEating);
         
         window.display();
         sf::sleep(sf::milliseconds(100));
     }
+    
     
     return EXIT_SUCCESS;
 }
