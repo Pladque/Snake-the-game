0bd51fc27def1a324ef155c71ff38a9c26d6dbd9
Pladque <platekmichal791@gmail.com>
2021-05-09 21:29:00 +0200

    partycle system start

---
+++ particle.cpp
@@ -0,0 +1,36 @@
+
+
+class Particle
+{
+    int speedX;
+    int speedY;
+    int ttl;
+    int size;
+    int color;
+    int velocityX;
+    int velocityY;
+
+    int positionX = 0, positionY = 0;
+
+    Particle(int newSpeedX, int newSpeedY, int newTTL, int newSize
+    ,int newColor, int newVelocityX, int newVelocityY)
+    {
+        this->speedX = newSpeedX;
+        this->speedY = newSpeedY;
+        this->ttl = newTTL;
+        this->size = newSize;
+        this->color = newColor;
+        this->velocityX = newVelocityX;
+        this->velocityY = newVelocityY;
+    }
+
+};
+
+
+class PartycleSystem
+{
+
+
+
+    
+}
