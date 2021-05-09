f2c0559423521d19b3d80a45620cb648226d7c92
Pladque <platekmichal791@gmail.com>
2021-05-09 20:51:26 +0200

    assets paths

--- consts.cpp
+++ consts.cpp
@@ -1,3 +1,12 @@
+#include <string>
+
+//zmiene w stlu GRID_SIZE_X bym wywali ldo pliku typu setting.cpp
+//a tutaj trzymal jakies takie wartosci ktorych nie bedizemy zmieniac w czasie
+// trwania programu
 enum Direction {up, down, left, right};
 const int GRID_SIZE_X = 30;
 const int GRID_SIZE_Y = 30;
+
+const std::string FONT_PATH = "/Assets/fonts";
+const std::string TEXTURES_PATH = "/Assets/Textures";
+const std::string SOUNDS_PATH = "/Assets/Sounds";
