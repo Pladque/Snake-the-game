dd6abe96f78884141910e640c42f0d0efedb75fd
wojtek777 <49556059+wojtek777@users.noreply.github.com>
2021-05-13 20:31:50 +0200

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -5,9 +5,13 @@
  */ 
 
 #define MULT 2
-#define REACT_X 28.f
-#define TEXT_X 30.f
-//#define REACT_Y
+#define REACT_X 25.f
+#define TEXT_X 26.f
+#define REACT_WIDTH 250.f
+#define REACT_HEIGHT 40.f
+#define CHAR_SIZE 20.f
+#define SPACING -60.f
+#define MOUSE_CORRECTION 25.f
 
 int difficulty = 1;			//0 - easy, 1 - normal, 2 - hard
 bool entered_settings = 0;	//value to determine if settings should be displayed on the screen
@@ -16,101 +20,124 @@ bool stay_in_menu = 1;		//may be used to determine if cotrol should stay in main
 
 int EnterMenu()
 {
+	//Main function -> depending on its return, game would start or not
 	std::string snakes_count = "1";
 	std::string difficulty_level = "normal";
 	std::string music_on_off = "on";
 	extern int snakeSpeed;
 	
-	//Main function -> depending on its return, game would start or not
+	
+	sf::Vector2i globalMousePosition;	//Variable holding mouse position;
 	sf::Texture menuTexture;
 	if (!menuTexture.loadFromFile(TEXTURES_PATH + "mainSnake2.jpg"))
 		return -1;
+	sf::Texture boardTexture;
 	sf::Music menuMusic;
 	if (!menuMusic.openFromFile(SOUNDS_PATH + "menuMusic.ogg"))
         return -1;
 	sf::RenderWindow menuWindow(sf::VideoMode(1280, 960), "SNAKE_2D");	//resolution may be changed, after every other thing works
 																		//as expected
 	sf::Sprite menuSprite(menuTexture);
+	sf::Sprite boardSprite(boardTexture);
 	sf::Event menuEvent;
 	sf::Font menuFont;
     if (!menuFont.loadFromFile("./Assets/Fonts/JosefinSans-SemiBoldItalic.ttf"))
         return -1;
-	sf::Text mainText, windStartGame, windQuitGame, windOptions, windTryAgain, windResign;	//text, displayed in consecutive windows
-	sf::Text windDifficultyLevel, windSnakesCount, windMusic, windReturnFromOptions;
+	sf::Text mainText, windStartGame, windQuitGame, windOptions, windTryAgain, windResign;	//text, displayed  
+	sf::Text windDifficultyLevel, windSnakesCount, windMusic, windReturnFromOptions;		//in consecutive windows
+	sf::Text windSelectBoard;
 													//end of Menu init
 													
-	sf::RectangleShape rectangleBasic(sf::Vector2f(232.f, 25.f));
+	sf::RectangleShape rectangleBasic(sf::Vector2f(REACT_WIDTH, REACT_HEIGHT));
     rectangleBasic.setFillColor(sf::Color(20, 100, 150));
-    rectangleBasic.setPosition(REACT_X, 115.f);
+    rectangleBasic.setPosition(REACT_X, 205.f + SPACING);
     rectangleBasic.setOutlineThickness(5.f);
 	rectangleBasic.setOutlineColor(sf::Color(250, 150, 100));
 	
-	sf::RectangleShape rectangleBasic2(sf::Vector2f(232.f, 25.f));
+	sf::RectangleShape rectangleBasic2(sf::Vector2f(REACT_WIDTH, REACT_HEIGHT));
     rectangleBasic2.setFillColor(sf::Color(20, 100, 150));
-    rectangleBasic2.setPosition(REACT_X, 178.f);		
+    rectangleBasic2.setPosition(REACT_X, 268.f + SPACING);		
     rectangleBasic2.setOutlineThickness(5.f);
 	rectangleBasic2.setOutlineColor(sf::Color(250, 150, 100));
     
-    sf::RectangleShape rectangleBasic3(sf::Vector2f(232.f, 25.f));
+    sf::RectangleShape rectangleBasic3(sf::Vector2f(REACT_WIDTH, REACT_HEIGHT));
     rectangleBasic3.setFillColor(sf::Color(20, 100, 150));
-    rectangleBasic3.setPosition(REACT_X, 241.f);		
+    rectangleBasic3.setPosition(REACT_X, 331.f + SPACING);		
     rectangleBasic3.setOutlineThickness(5.f);
 	rectangleBasic3.setOutlineColor(sf::Color(250, 150, 100));		
 	
-	sf::RectangleShape rectangleBasic4(sf::Vector2f(232.f, 25.f));
+	sf::RectangleShape rectangleBasic4(sf::Vector2f(REACT_WIDTH, REACT_HEIGHT));
     rectangleBasic4.setFillColor(sf::Color(20, 100, 150));
-    rectangleBasic4.setPosition(REACT_X, 304.f);		
+    rectangleBasic4.setPosition(REACT_X, 394.f + SPACING);		
     rectangleBasic4.setOutlineThickness(5.f);
-	rectangleBasic4.setOutlineColor(sf::Color(250, 150, 100));								
+	rectangleBasic4.setOutlineColor(sf::Color(250, 150, 100));	
+	
+	sf::RectangleShape rectangleBasic5(sf::Vector2f(REACT_WIDTH, REACT_HEIGHT));
+    rectangleBasic5.setFillColor(sf::Color(20, 100, 150));
+    rectangleBasic5.setPosition(REACT_X, 457.f + SPACING);		
+    rectangleBasic5.setOutlineThickness(5.f);
+	rectangleBasic5.setOutlineColor(sf::Color(250, 150, 100));		
+	
+	sf::RectangleShape rectangleBoard(sf::Vector2f(REACT_WIDTH, REACT_WIDTH));
+    rectangleBoard.setFillColor(sf::Color(255, 255, 255));
+    rectangleBoard.setPosition(REACT_X, 520.f + SPACING);		
+    //rectangleBasic5.setOutlineThickness(5.f);
+	//rectangleBasic5.setOutlineColor(sf::Color(250, 150, 100));					
 													
 	mainText.setFont(menuFont);
 	mainText.setString("SNAKE 2D");
-	mainText.setCharacterSize(36);
+	mainText.setCharacterSize(38);
 	mainText.setFillColor(sf::Color::Red);
 	mainText.setPosition(570.f, 5.f);
 	mainText.setStyle(sf::Text::Bold);
 	
 	windStartGame.setFont(menuFont);
 	windStartGame.setString("New game: press 'P'");
-	windStartGame.setCharacterSize(18);
+	windStartGame.setCharacterSize(CHAR_SIZE);
 	windStartGame.setFillColor(sf::Color::Black);
-	windStartGame.setPosition(TEXT_X, 116.f);
+	windStartGame.setPosition(TEXT_X, 209.f + SPACING);
 	
 	windOptions.setFont(menuFont);
 	windOptions.setString("Options: press 'S'");
-	windOptions.setCharacterSize(18);
+	windOptions.setCharacterSize(CHAR_SIZE);
 	windOptions.setFillColor(sf::Color::Black);
-	windOptions.setPosition(TEXT_X, 179.f);
+	windOptions.setPosition(TEXT_X, 272.f + SPACING);
 	
 	windQuitGame.setFont(menuFont);
 	windQuitGame.setString("Quit game: press 'Q'");
-	windQuitGame.setCharacterSize(18);
+	windQuitGame.setCharacterSize(CHAR_SIZE);
 	windQuitGame.setFillColor(sf::Color::Black);
-	windQuitGame.setPosition(TEXT_X, 242.f);
+	windQuitGame.setPosition(TEXT_X, 335.f + SPACING);
 	
 	windSnakesCount.setFont(menuFont);
 	windSnakesCount.setString("Snakes number: " + snakes_count + " ('N')");
-	windSnakesCount.setCharacterSize(18);
+	windSnakesCount.setCharacterSize(CHAR_SIZE);
 	windSnakesCount.setFillColor(sf::Color::Black);
-	windSnakesCount.setPosition(TEXT_X, 116.f);
+	windSnakesCount.setPosition(TEXT_X, 209.f + SPACING);
 	
 	windDifficultyLevel.setFont(menuFont);
 	windDifficultyLevel.setString("Difficulty level: " + difficulty_level + " ('L')");
-	windDifficultyLevel.setCharacterSize(18);
+	windDifficultyLevel.setCharacterSize(CHAR_SIZE);
 	windDifficultyLevel.setFillColor(sf::Color::Black);
-	windDifficultyLevel.setPosition(TEXT_X, 179.f);
+	windDifficultyLevel.setPosition(TEXT_X, 272.f + SPACING);
 	
 	windMusic.setFont(menuFont);
 	windMusic.setString("Music: " + music_on_off + " ('M')");
-	windMusic.setCharacterSize(18);
+	windMusic.setCharacterSize(CHAR_SIZE);
 	windMusic.setFillColor(sf::Color::Black);
-	windMusic.setPosition(TEXT_X, 242.f);
+	windMusic.setPosition(TEXT_X, 335.f + SPACING);
 	
 	windReturnFromOptions.setFont(menuFont);
 	windReturnFromOptions.setString("Return to menu ('Esc')");
-	windReturnFromOptions.setCharacterSize(18);
+	windReturnFromOptions.setCharacterSize(CHAR_SIZE);
 	windReturnFromOptions.setFillColor(sf::Color::Black);
-	windReturnFromOptions.setPosition(TEXT_X, 305.f);
+	windReturnFromOptions.setPosition(TEXT_X, 398.f + SPACING);
+	
+	windSelectBoard.setFont(menuFont);
+	windSelectBoard.setString("Select board: ('V')");
+	windSelectBoard.setCharacterSize(CHAR_SIZE);
+	windSelectBoard.setFillColor(sf::Color::Black);
+	windSelectBoard.setPosition(TEXT_X, 461.f + SPACING);
 	
 	
 	menuMusic.play(); 
@@ -127,10 +154,10 @@ int EnterMenu()
 				menuWindow.close();
 			else if (menuEvent.type == sf::Event::KeyPressed)
 			{
-				if ((menuEvent.key.code == sf::Keyboard::Q || menuEvent.key.code == sf::Keyboard::Q) && entered_settings == 0)
+				if ((menuEvent.key.code == sf::Keyboard::Q || menuEvent.key.code == sf::Keyboard::Escape) && entered_settings == 0)
 			    {
 			        //the escape key was pressed
-			        //Q is used to exit game
+			        //Q and Esc are used to exit game
 			        menuWindow.close();
 			        return -1;
 			    }
@@ -192,6 +219,90 @@ int EnterMenu()
 					continue;
 				}
 			}
+			else if (sf::Mouse::isButtonPressed(sf::Mouse::Left))
+			{
+			    // left mouse button is pressed
+			    globalMousePosition = sf::Mouse::getPosition();
+			    if (globalMousePosition.x >= REACT_X && globalMousePosition.x <= (REACT_X + REACT_WIDTH))
+			    {
+					if (globalMousePosition.y >= (205.f + SPACING + MOUSE_CORRECTION) && globalMousePosition.y <= (205.f + SPACING + MOUSE_CORRECTION + REACT_HEIGHT) && entered_settings == 0)
+					{
+						//Start the game window was clicked by mouse
+						menuWindow.close();
+						return 0;
+					}
+					else if (globalMousePosition.y >= (268.f + SPACING + MOUSE_CORRECTION) && globalMousePosition.y <= (268.f + SPACING + MOUSE_CORRECTION + REACT_HEIGHT) && entered_settings == 0)
+					{
+						//settings window was clicked by mouse
+						entered_settings = 1;
+					}
+					else if (globalMousePosition.y >= (331.f + SPACING + MOUSE_CORRECTION) && globalMousePosition.y <= (331.f + SPACING + MOUSE_CORRECTION + REACT_HEIGHT) && entered_settings == 0)
+					{
+						//quit game was clicked by mouse
+				        menuWindow.close();
+				        return -1;
+					}
+					else if (globalMousePosition.y >= (205.f + SPACING + MOUSE_CORRECTION) && globalMousePosition.y <= (205.f + SPACING + MOUSE_CORRECTION + REACT_HEIGHT) && entered_settings == 1)
+					{
+						//snakes_count/apple_counts window
+					}
+					else if (globalMousePosition.y >= (268.f + SPACING + MOUSE_CORRECTION) && globalMousePosition.y <= (268.f + SPACING + MOUSE_CORRECTION + REACT_HEIGHT) && entered_settings == 1)
+					{
+						//difficulty window was cliked by mouse
+						difficulty++; 
+						difficulty = difficulty % 3;
+						if (difficulty == 0)
+						{
+							difficulty_level = "easy";
+							snakeSpeed = 9;
+							windDifficultyLevel.setString("Difficulty level: " + difficulty_level + " ('L')");
+						}
+						else if (difficulty == 1)
+						{
+							difficulty_level = "normal";
+							snakeSpeed = 7;
+							windDifficultyLevel.setString("Difficulty level: " + difficulty_level + " ('L')");
+						}
+						else
+						{
+							difficulty_level = "hard";
+							snakeSpeed = 4;
+							windDifficultyLevel.setString("Difficulty level: " + difficulty_level + " ('L')");
+						}
+					}
+					else if (globalMousePosition.y >= (331.f + SPACING + MOUSE_CORRECTION) && globalMousePosition.y <= (331.f + SPACING + MOUSE_CORRECTION + REACT_HEIGHT) && entered_settings == 1)
+					{
+						//music window was clicked by mouse
+						if (music_on_off == "on")
+						{
+							music_on_off = "off";
+							windMusic.setString("Music: " + music_on_off + " ('M')");
+							menuMusic.stop();
+						}
+						else
+						{
+							music_on_off = "on";
+							windMusic.setString("Music: " + music_on_off + " ('M')");
+							menuMusic.play();
+							menuMusic.setVolume(50.f);
+						}
+					}
+					else if (globalMousePosition.y >= (394.f + SPACING + MOUSE_CORRECTION) && globalMousePosition.y <= (394.f + SPACING + MOUSE_CORRECTION + REACT_HEIGHT) && entered_settings == 1)
+					{
+						//Return to menu was clicked by mouse
+						entered_settings = 0;
+					}
+					else if (globalMousePosition.y >= (457.f + SPACING + MOUSE_CORRECTION) && globalMousePosition.y <= (457.f + SPACING + MOUSE_CORRECTION + REACT_HEIGHT) && entered_settings == 1)
+					{
+						//Select board was clicked by mouse
+					}
+					else
+					{
+						//nothing meaningful happened; proceed with loop 
+						continue;
+					}
+				}
+			}
         }
         menuWindow.clear();
         menuWindow.draw(menuSprite);
@@ -208,10 +319,13 @@ int EnterMenu()
 		else
 		{
 			menuWindow.draw(rectangleBasic4);
+			menuWindow.draw(rectangleBasic5);
+			menuWindow.draw(rectangleBoard);
 			menuWindow.draw(windDifficultyLevel);
 			menuWindow.draw(windSnakesCount);
 	        menuWindow.draw(windReturnFromOptions);
 	        menuWindow.draw(windMusic);
+	        menuWindow.draw(windSelectBoard);
 		}
         menuWindow.display();
 	}
